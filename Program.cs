8c3a96aafa514f14ee1ba4060051d249f6ab4148
Michael Bender <mbender@primus-tech.com>
2019-08-20 16:42:52 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                foreach(var name in args)
+                {
+                    Console.WriteLine("Hello " + name + "!");
+                }               
+            }
+            else 
+                System.Console.WriteLine("No name specified.");
+        }
+    }
+}
